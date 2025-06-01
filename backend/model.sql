18bd24abc76a695f190b0de94cd5b80e42fdd48f
guiibrag4 <guilhermebragaepic2@gmail.com>
2025-06-01 02:11:47 -0300

    [main] adicionando model do banco para registro e removendo um arquivo desnecessário

---
+++ backend/model.sql
@@ -0,0 +1,46 @@
+CREATE TABLE usuario (
+    id_usuario SERIAL PRIMARY KEY,
+    nome VARCHAR (75) NOT NULL,
+    email VARCHAR (150) NOT NULL UNIQUE,
+    senha_hash TEXT,
+    data_nasc DATE NOT NULL,
+    google_id VARCHAR (50) UNIQUE,
+    data_criacao TIMESTRAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    ultimo_login TIMESTAMP WITH TIME ZONE
+)
+
+CREATE TABLE app_biblia.PaginasSalvas (
+    id_pagina_salva SERIAL PRIMARY KEY,
+    id_usuario INTEGER REFERENCES app_biblia.Usuario(id_usuario) ON DELETE CASCADE,
+    livro_abreviacao VARCHAR(5) NOT NULL,
+    capitulo INTEGER NOT NULL,
+    versao_biblia VARCHAR(10) NOT NULL,
+    data_salvo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (id_usuario, livro_abreviacao, capitulo, versao_biblia)
+);
+
+CREATE TABLE app_biblia.Grifos (
+    id_grifo SERIAL PRIMARY KEY,
+    id_usuario INTEGER REFERENCES app_biblia.Usuario(id_usuario) ON DELETE CASCADE,
+    livro_abreviacao VARCHAR(5) NOT NULL,
+    capitulo INTEGER NOT NULL,
+    versiculo_numero INTEGER NOT NULL,
+    cor_grifo VARCHAR(20) NOT NULL,
+    versao_biblia VARCHAR(10) NOT NULL,
+    texto_grifado TEXT,
+    data_grifado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (id_usuario, livro_abreviacao, capitulo, versiculo_numero, versao_biblia, cor_grifo) -- Considera a cor para permitir grifos diferentes no mesmo versículo
+);
+
+CREATE TABLE app_biblia.Anotacoes (
+    id_anotacao SERIAL PRIMARY KEY,
+    id_usuario INTEGER REFERENCES app_biblia.Usuario(id_usuario) ON DELETE CASCADE,
+    livro_abreviacao VARCHAR(5) NOT NULL,
+    capitulo INTEGER NOT NULL,
+    versiculo_numero INTEGER NOT NULL,
+    texto_anotacao TEXT NOT NULL,
+    versao_biblia VARCHAR(10) NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_modificacao TIMESTAMP,
+    UNIQUE (id_usuario, livro_abreviacao, capitulo, versiculo_numero, versao_biblia) -- Um usuário só pode ter uma anotação por versículo/versão
+);
