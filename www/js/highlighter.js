ecd3d6f6fb274524642de1fa24974f0d6d0191c8
Guilherme Braga Rios da Costa <guilhermebragaepic2@gmail.com>
2025-10-23 11:44:56 -0300

    Correção no save de páginas

--- www/js/highlighter.js
+++ www/js/highlighter.js
@@ -699,8 +699,8 @@ class PageSaver {
     constructor() {
         this.bookAbbreviations = {
             "Gênesis": "gn", "Êxodo": "ex", "Levítico": "lv", "Números": "nm", "Deuteronômio": "dt",
-            "Josué": "js", "Juízes": "jz", "Rute": "rt", "1 Samuel": "1sm", "2 Samuel": "2sm",
-            "1 Reis": "1rs", "2 Reis": "2rs", "1 Crônicas": "1cr", "2 Crônicas": "2cr",
+            "Josué": "js", "Juízes": "jz", "Rute": "rt", "1° Samuel": "1sm", "2° Samuel": "2sm",
+            "1° Reis": "1rs", "2° Reis": "2rs", "1° Crônicas": "1cr", "2° Crônicas": "2cr",
             "Esdras": "ed", "Neemias": "ne", "Ester": "et", "Jó": "jó", "Salmos": "sl",
             "Provérbios": "pv", "Eclesiastes": "ec", "Cantares": "ct", "Isaías": "is",
             "Jeremias": "jr", "Lamentações": "lm", "Ezequiel": "ez", "Daniel": "dn",
@@ -708,12 +708,12 @@ class PageSaver {
             "Miquéias": "mq", "Naum": "na", "Habacuque": "hb", "Sofonias": "sf",
             "Ageu": "ag", "Zacarias": "zc", "Malaquias": "ml", "Mateus": "mt",
             "Marcos": "mc", "Lucas": "lc", "João": "jo", "Atos": "at", "Romanos": "rm",
-            "1 Coríntios": "1co", "2 Coríntios": "2co", "Gálatas": "gl",
+            "1° Coríntios": "1co", "2° Coríntios": "2co", "Gálatas": "gl",
             "Efésios": "ef", "Filipenses": "fp", "Colossenses": "cl",
-            "1 Tessalonicenses": "1ts", "2 Tessalonicenses": "2ts",
-            "1 Timóteo": "1tm", "2 Timóteo": "2tm", "Tito": "tt", "Filemom": "fl",
-            "Hebreus": "hb", "Tiago": "tg", "1 Pedro": "1pe", "2 Pedro": "2pe",
-            "1 João": "1jo", "2 João": "2jo", "3 João": "3jo", "Judas": "jd",
+            "1° Tessalonicenses": "1ts", "2° Tessalonicenses": "2ts",
+            "1° Timóteo": "1tm", "2° Timóteo": "2tm", "Tito": "tt", "Filemom": "fl",
+            "Hebreus": "hb", "Tiago": "tg", "1° Pedro": "1pe", "2° Pedro": "2pe",
+            "1° João": "1jo", "2° João": "2jo", "3° João": "3jo", "Judas": "jd",
             "Apocalipse": "ap"
         };
         this.init();
